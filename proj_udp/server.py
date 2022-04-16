e0fd0ca6d87f65b70ac2fd3c93472f545922633a
Henrique <hgo@cin.ufpe.br>
2022-04-16 14:01:18 -0300

    refactoring

--- proj_udp/server.py
+++ proj_udp/server.py
@@ -11,7 +11,7 @@ class Server(Thread):
 
 
     def run(self):
-        game = GameMessages(serverIpAddress = self.serverAddress)
+        game = GameMessages(serverIpAddress = self.serverAddress, serverSocket= self.server)
         while True:
             bytesMessage, clientAddress = self.server.recvfrom(2048)
             if self.stopServer:
