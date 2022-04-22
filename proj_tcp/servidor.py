00c04168cab2546433b57c437d420dd5ae5d5ea0
barbaravazferreira <barbara01052002@gmail.com>
2022-04-22 16:18:25 -0300

    servidor com correcao dos erros de requisicao

--- proj_tcp/servidor.py
+++ proj_tcp/servidor.py
@@ -11,8 +11,26 @@ def servidor(socket_client):
         if not data:
             break
         data_decodif = data.decode()
-        metodo = data_decodif.split(' ')[0]
-        requisicao = data_decodif.split(' ')[1]
+        lista_requisicao = data_decodif.split(' ')
+        print(lista_requisicao)
+        if len(lista_requisicao) < 3 or lista_requisicao[0] != 'GET':
+            header = 'HTTP/1.1 400 Bad Request\n\n'
+            arquivo_lido = '<html><body><center><h3>Error 400: Bad Request</h3><p>Mensagem de requisicao nao entendida pelo servidor</p></center></body></html>'
+            arquivo_lido = arquivo_lido.encode('utf-8')
+            resposta_final = header.encode('utf-8')
+            resposta_final += arquivo_lido
+            socket_client.send(resposta_final)
+            break
+        if lista_requisicao[2] != 'HTTP/1.1\r\nHost:':
+            header = 'HTTP/1.1 505 HTTP Version Not Supported\n\n'
+            arquivo_lido = '<html><body><center><h3>Error 505: HTTP Version Not Supported</h3><p>Versao do HTTP utilizada não é suportada neste servidor</p></center></body></html>'
+            arquivo_lido = arquivo_lido.encode('utf-8')
+            resposta_final = header.encode('utf-8')
+            resposta_final += arquivo_lido
+            socket_client.send(resposta_final)
+            break
+        metodo = lista_requisicao[0]
+        requisicao = lista_requisicao[1]
         print('Requisição do cliente:', requisicao, metodo)
         meu_arquivo = requisicao.lstrip('/')
         if meu_arquivo == '':
@@ -23,13 +41,13 @@ def servidor(socket_client):
             arquivo.close()
             if meu_arquivo.endswith('.html'):
                 mimetype = 'text/html'
-            elif meu_arquivo.endswith('.htm'): #nao testei
+            elif meu_arquivo.endswith('.htm'):
                 mimetype = 'text/htm'
             elif meu_arquivo.endswith('.txt'):
                 mimetype = 'text/txt'
             elif meu_arquivo.endswith('.css'):
                 mimetype = 'text/css'
-            elif meu_arquivo.endswith('.js'): #nao está dando muito bom
+            elif meu_arquivo.endswith('.js'):
                 mimetype = 'aplication/js'
             elif meu_arquivo.endswith('.pdf'):
                 mimetype = 'application/pdf'
@@ -44,7 +62,6 @@ def servidor(socket_client):
             header = 'HTTP/1.1 200 OK\n'
             header += 'Content-Type: ' + str(mimetype) + '\n\n'
         except:
-            #erros 400 Bad Request, 404 ou 505 HTTP Version Not Supported
             header = 'HTTP/1.1 404 Not Found\n\n'
             arquivo_lido = '<html><body><center><h3>Error 404: File not found</h3><p>Documento requisitado nao foi localizado no servidor</p></center></body></html>'
             arquivo_lido = arquivo_lido.encode('utf-8')
@@ -64,7 +81,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
 
 
 
