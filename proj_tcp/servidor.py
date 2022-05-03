77f82f6c64e016dcdc0ddaed8a0cced965edc655
Dante Santana <dantebsantana@gmail.com>
2022-05-03 15:50:50 -0300

    atualização final

--- proj_tcp/servidor.py
+++ proj_tcp/servidor.py
@@ -15,7 +15,7 @@ def servidor(socket_client):
             break
         data_decodif = data.decode()
         lista_requisicao = data_decodif.split(' ')
-        print(lista_requisicao)
+        #print(lista_requisicao)
         if len(lista_requisicao) < 3 or lista_requisicao[0] != 'GET':
             header = 'HTTP/1.1 400 Bad Request\n\n'
             arquivo_lido = '<html><body><center><h3>Error 400: Bad Request</h3><p>Mensagem de requisicao nao entendida pelo servidor</p></center></body></html>'
@@ -57,6 +57,8 @@ def servidor(socket_client):
                 mimetype = 'image/png'
             elif meu_arquivo.endswith('.gif'):
                 mimetype = 'image/gif'
+            elif meu_arquivo.endswith('.jpeg'):
+                mimetype = 'image/jpeg'
             else:
                 mimetype = 'image/jpg'
 
@@ -68,23 +70,37 @@ def servidor(socket_client):
             header += 'Content-Type: ' + str(mimetype) + '\n\n'
 
         except:
-            if os.path.isdir(meu_arquivo): #nagecacao
-                if meu_arquivo[-1] == "/":
-                    meu_arquivo = meu_arquivo[:-1]
 
-                voltar = "/" + meu_arquivo
-                voltar_url = ""
-                voltar_split = voltar.split("/")
-                voltar_split.remove(voltar_split[-1])
-                for x in voltar_split:
-                    voltar_url += x + "/"
+            if os.path.isdir(meu_arquivo):
+                print(requisicao, ' <- requisição')
+                if requisicao[-1] != "/":
+                    requisicao = requisicao + "/"
+
+                rota = requisicao.split("/")
+                rota = list(filter(None, rota))
+                rota.pop(-1)
+                voltar_url = "/"
+                for x in rota:
+                    y = x + "/"
+                    voltar_url += y
+                print(voltar_url)
                 variavel = '<a href="' + voltar_url + '">Voltar</a>'
+                print(requisicao)
                 for x in os.listdir(meu_arquivo):
-                    local = meu_arquivo + "/" + x
+                    print(x)
+                    local = requisicao + x
+                    try:
+                        y = str(round(os.path.getsize(local) / 1024))
+                        z = str(datetime.fromtimestamp(os.path.getmtime(local)))
+                        variavel += "<p><a href=" + local + " title= \"link\"target=\"_blank\">" + x + "</a> - Tamanho: <span style='color: red;'> " + y + "kb - </span><span style='color: black;'> Ultima alteracao: </span><span style='color: green;'>" + z + "</span></p>"
+                    except Exception as e:
+                        print(e)
+                        local = str(local).removeprefix("/")
+                        y = str(round(os.path.getsize(local) / 1024))
+                        z = str(datetime.fromtimestamp(os.path.getmtime(local)))
+                        local = "/" + local
+                        variavel += "<p><a href=" + local + " title= \"link\"target=\"_blank\">" + x + "</a> - Tamanho: <span style='color: red;'> " + y + "kb - </span><span style='color: black;'> Ultima alteracao: </span><span style='color: green;'>" + z + "</span></p>"
 
-                    y = str(round(os.path.getsize(local)/1024))
-                    z = str(datetime.fromtimestamp(os.path.getmtime(local)))
-                    variavel += "<p><a href=" + local + " title= \"link\"target=\"_blank\">" + x + "</a> - Tamanho: <span style='color: red;'> " + y + "kb - ultima alteracao: </span><span style='color: green;'>" + z +"</span></p>"
 
                 header = 'HTTP/1.1 200 OK\n'
                 header += 'Content-Type: ' + "text/html" + '\n\n'
@@ -94,6 +110,7 @@ def servidor(socket_client):
                 arquivo_lido = str(arquivo_lido).replace("{variavel}", variavel)
                 arquivo_lido = str(arquivo_lido).replace("{caminho}", meu_arquivo)
                 arquivo_lido = arquivo_lido.encode("utf-8")
+
             else:
                 header = 'HTTP/1.1 404 Not Found\n\n'
                 arquivo_lido = '<html><body><center><h3>Error 404: File not found</h3><p>Documento requisitado nao foi localizado no servidor</p></center></body></html>'
@@ -106,19 +123,19 @@ def servidor(socket_client):
 
 
 def main():
-    socket_server = socket(AF_INET, SOCK_STREAM)
-    socket_server.bind((host, porta))
-    socket_server.listen(3)
 
     if not os.path.exists("pasta"):
         os.makedirs("pasta")
         print("criou a pasta")
 
+    socket_server = socket(AF_INET, SOCK_STREAM)
+    socket_server.bind((host, porta))
+    socket_server.listen(3)
+
     while True:
         socket_client, endereco_cliente = socket_server.accept()
         start_new_thread(servidor, (socket_client,))
         print(f'Conectado com: {endereco_cliente[0]}:{endereco_cliente[1]}')
-
 
 if __name__ == "__main__":
     main()
